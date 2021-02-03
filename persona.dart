dbfe1ae2971904ee997ffbdb2d1c86be5e9f32ae
diansp07 <78125195+diansp07@users.noreply.github.com>
2021-02-03 12:21:51 -0800

    Add files via upload

---
+++ persona.dart
@@ -0,0 +1,18 @@
+class Persona {
+  String _nombre;
+  int _edad;
+
+  //Persona(this._nombre, this._edad);
+
+  set nombre(String nom) {
+    this._nombre = nom;
+  }
+
+  set edad( int ed ){
+    this._edad = ed;
+  }
+
+  String get nombrePersona{
+    return _nombre;
+  }
+}
